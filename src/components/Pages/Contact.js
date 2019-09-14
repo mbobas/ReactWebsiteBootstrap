8244724910262eb68d1faed460b2373c3a6abae5
mbobas <michal.gulczynski88@gmail.com>
2019-09-14 16:06:10 +0200

    commit UploadedContactStates

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -2,6 +2,19 @@ import React, { Component} from 'react';
 
 
 class Contact extends Component {
+    constructor(props){
+        super(props);
+
+        this.state ={
+            name: '',
+            email: '',
+            phone: '',
+            message: ''
+        }
+
+    }
+
+
     render(){
         return(
             
@@ -19,21 +32,54 @@ class Contact extends Component {
                     <div className="row">
                     <div className="col-md-6">
                         <div className="form-group">
-                        <input className="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name."/>
+                        <input 
+                            className="form-control" 
+                            id="name" type="text" 
+                            placeholder="Your Name *" 
+                            required="required" 
+                            data-validation-required-message="Please enter your name."
+                            value={this.state.name}
+                            onChange={e => this.setState({name: e.target.value})}
+                        />
                         <p className="help-block text-danger"></p>
                         </div>
                         <div className="form-group">
-                        <input className="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address." />
+                            <input 
+                                className="form-control" 
+                                id="email" type="email" 
+                                placeholder="Your Email *" 
+                                required="required" 
+                                data-validation-required-message="Please enter your email address." 
+                                value={this.state.email}
+                                onChange={e => this.setState({email: e.target.value})}
+                            />
                         <p className="help-block text-danger"></p>
                         </div>
                         <div className="form-group">
-                        <input className="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number."/>
+                            <input 
+                                className="form-control" 
+                                id="phone" type="tel" 
+                                placeholder="Your Phone *" 
+                                required="required" 
+                                data-validation-required-message="Please enter your phone number."
+                                value={this.state.phone}
+                                onChange={e => this.setState({phone: e.target.value})}
+                            />
                         <p className="help-block text-danger"></p>
                         </div>
                     </div>
                     <div className="col-md-6">
                         <div className="form-group">
-                        <textarea className="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
+                            <textarea
+                                className="form-control" 
+                                id="message" 
+                                placeholder="Your Message *" 
+                                required="required" 
+                                data-validation-required-message="Please enter a message."
+                                value={this.state.message}
+                                onChange={e => this.setState({message: e.target.value})}
+
+                                />
                         <p className="help-block text-danger"></p>
                         </div>
                     </div>
